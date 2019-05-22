7c750d0e0aa3a465c6d78c1f3912f8803e59acf6
Ajulu Adigwe <ajuluadigwe@zipcoders-MacBook-Pro-2.local>
2019-05-22 13:26:13 -0400

    successful

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
+ * Write a program which prompts the user to input a number, n.
+ * The program should respond by printing the sum of the numbers 1 to n.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner game = new Scanner (System.in);
+        int guess, sum;
+        System.out.println("Hello! Type a number");
+        guess = game.nextInt();
+        sum = guess + 1;
+        
+        System.out.println(sum);
+        
 
     }
 }
